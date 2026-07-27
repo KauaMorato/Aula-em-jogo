11bf1c4e9060abe315be8af5be87779c91c4af39
Matheus Sampaio <matheusampaio098@gmail.com>
2026-07-27 10:21:15 -0300

    Adiciona tela de login

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const menuBtn = document.getElementById('menuBtn');
 const navMenu = document.getElementById('navMenu');
+
+function mostrarLogin() {
+    document.getElementById("login").style.display = "block";
+}
+
+function entrar() {
+
+    let usuario = document.getElementById("usuario").value;
+
+    if (usuario === "") {
+        alert("Digite um nome!");
+        return;
+    }
+
+    alert("Bem-Vindo, " + usuario + "!");
+
+    localStorage.setItem("usuario", usuario);
+}
