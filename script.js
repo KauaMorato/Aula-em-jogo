7ce8810ad5475ddabcae2f587b39e70c39dd9319
Matheus Cavalcante Sampaio <matheusampaio098@gmail.com>
2026-07-28 11:37:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -21,3 +21,41 @@ function entrar() {
 
     document.getElementById("bemVindo").textContent = "Bem-vindo, " + usuario + "!";
 }
+
+//Movimento do personagem provisório
+
+let posX = 50;
+let posY = 50;
+
+document.addEventListener("keydown", function(event){
+
+    if (
+        event.key === "ArrowUp" ||
+        event.key === "ArrowDown" ||
+        event.key === "ArrowLeft" ||
+        event.key === "ArrowRight"
+    ) {
+        event.preventDefault();
+    }
+
+    const player = document.getElementById("player");
+
+    if(event.key === "ArrowRight" && posX < 760){
+        posX += 10;
+    }
+
+    if(event.key === "ArrowLeft" && posX > 0){
+        posX -= 10;
+    }
+
+    if(event.key === "ArrowUp" && posY > 0){
+        posY -= 10;
+    }
+
+    if(event.key === "ArrowDown" && posY < 410){
+        posY += 10;
+    }
+
+    player.style.left = posX + "px";
+    player.style.top = posY + "px";
+});
