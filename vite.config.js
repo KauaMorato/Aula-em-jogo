f2bca8e29c75f149f790c257d242d40bc05ff35a
Kauã Morato <dxrkitslove4@gmail.com>
2026-09-17 18:33:51 -0300

    arrumando a base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: './',
+  base: '/Aula-em-jogo/',
   root: 'Front-End',
   envDir: '../',
   build: {
