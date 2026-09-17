dc9b4e0e565d9c797f8442539d0e125aad5163ba
Kauã Morato <dxrkitslove4@gmail.com>
2026-09-17 18:16:48 -0300

    Arrumando o vite e removendo comentário

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: './',
   root: 'Front-End',
-  envDir: '../', // <--- Adicione esta linha
+  envDir: '../',
   build: {
     outDir: '../dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'Front-End/index.html'),
+        documentos: resolve(__dirname, 'Front-End/documentos.html') // Adicione esta linha
+      }
+    }
   }
 });
